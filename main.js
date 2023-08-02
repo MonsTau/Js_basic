39fc73fe7c557c87bd7c6f45872b5ebabe84982a
MonsTau <nahanda@naver.com>
2023-08-02 11:56:57 +0900

    DOM 스타일링 & 속성제어

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-/* 
-    e.target: 이벤트가 발생했을때 마우스 포인터 위치 요소값 반환
-    e.currentTarget: 이벤트가 발생했을때 선택자 요소를 반환
-*/
+const box = document.querySelector('.box');
 
-const main = document.querySelector('main');
-
-/* main.addEventListener('click', e => {
-    console.log(e.currentTarget);  // <main>...</main>
-}) */
-
-main.addEventListener('click', e => {
-    console.log(e.target);
+box.addEventListener('click', e => {
+    e.currentTarget.style.backgroundColor = 'hotpink';
+    e.currentTarget.style.width = 200+'px';
+    console.log(getComputedStyle(e.currentTarget).width);
 })
+
+const box2 = document.querySelector('.box2');
+
+box2.addEventListener('click', e => {
+    /*  const style = getComputedStyle(e.currentTarget);
+    console.log(style['background-color']);   or  */
+    const bg = getComputedStyle(e.currentTarget).backgroundColor;
+    const wid = getComputedStyle(e.currentTarget).width;
+    console.log(bg);
+    console.log(wid);
+})
+
+const a = document.querySelector('a');
+
+console.log(a.getAttribute('href'));
+console.log(a.getAttribute('target'));
+console.log(a.getAttribute('title'));
+
+a.setAttribute('href', 'http://www.nate.com')
+a.setAttribute('target', '_self');
+a.setAttribute('title', '네이트로 이동');
+a.innerText = '네이트';
+
+console.log(a.getAttribute('href'));
+console.log(a.getAttribute('target'));
+console.log(a.getAttribute('title'));
